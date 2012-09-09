a092d47ba33060b4c0987be388607aed74dc4968
Asger Hallas <ahl@asgerhallas.dk>
2012-09-09 23:27:09 +0200

    initial

---
+++ Pollka.Tests/QueueTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Pollka.Tests
+{
+    public class QueueTests
+    {
+        [Test]
+        public void ReceivesSubscribedMessage()
+        {
+            object receivedMessage = null;
+            var queue = new Queue();
+            queue.Receive("channel", message => receivedMessage = message);
+            queue.Send("channel", new { Something = 1 });
+            receivedMessage.ShouldNotBe(null);
+        }
+
+        [Test]
+        public void ReceivesOnlyOneMessagePerCallToRecieve()
+        {
+            var receivedMessages = new List<object>();
+            var queue = new Queue();
+            queue.Receive("channel", receivedMessages.Add);
+            queue.Send("channel", new { Something = 1 });
+            queue.Send("channel", new { Something = 2 });
+            receivedMessages.Count.ShouldBe(1);
+        }
+
+        [Test]
+        public void DoesNotReceiveMessageOnOtherChannel()
+        {
+            object receivedMessage = null;
+            var queue = new Queue();
+            queue.Receive("channel1", message => receivedMessage = message);
+            queue.Send("channel2", new { Something = 1 });
+            receivedMessage.ShouldBe(null);
+        }
+    }
+}
---
+++ Pollka/Queue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Pollka
+{
+    public class Queue
+    {
+        readonly ISubject<InternalMessage> messages = new Subject<InternalMessage>();
+        
+        public void Send(string channel, object message)
+        {
+            messages.OnNext(new InternalMessage(new List<string> { channel }, message));
+        }
+
+        public void Receive(string channel, Action<object> callback)
+        {
+            messages
+                .Where(message => message.Channels.Contains(channel))
+                .Subscribe(message => callback(message.Message));
+        }
+
+        class InternalMessage
+        {
+            public InternalMessage(List<string> channels, object message)
+            {
+                Channels = channels;
+                Message = message;
+            }
+
+            public List<string> Channels { get; private set; }
+            public object Message { get; private set; }
+        }
+    }
+}
